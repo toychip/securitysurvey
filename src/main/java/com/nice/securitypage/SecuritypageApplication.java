96248e3f34a03e6efd466e457dc3478036803110
toychip <dlawnsgud427@naver.com>
2023-07-10 21:18:00 +0900

    StartNiceProject

---
+++ src/main/java/com/nice/securitypage/SecuritypageApplication.java
@@ -0,0 +1,13 @@
+package com.nice.securitypage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritypageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritypageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nice/securitypage/SecuritypageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nice.securitypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritypageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
